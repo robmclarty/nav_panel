3300f976964fdce9ee7f772fa421366993a8050f
Rob McLarty <rob.mclarty@gmail.com>
2013-06-06 16:08:50 -0400

    create some basic tests of helper methods

--- test/action_view_extensions_test.rb
+++ test/action_view_extensions_test.rb
@@ -2,45 +2,69 @@ require 'test_helper'
 
 class ActionViewExtensionsTest < ActiveSupport::TestCase
   include AdminNav::Rails::ActionViewExtensions
+  include ActionView::Helpers::UrlHelper
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::CaptureHelper
+  include ActionView::Context
+
+  def setup
+    @sample_link_label = 'Label'
+    @sample_link_url = 'http://somewhere.com'
+  end
 
   test "insert_admin_button_class_to adds admin-button class if no existing classes" do
-    flunk 'Not done testing yet.'
+    result = insert_admin_button_class_to({})
+    
+    assert_equal({ :class => "admin-button" }, result)
   end
 
   test "insert_admin_button_class_to concats admin-button class if already existing classes" do
-    flunk 'Not done testing yet.'
+    result = insert_admin_button_class_to({ :class => 'existing-class' })
+    
+    assert_equal({ :class => "existing-class admin-button" }, result)
   end
 
   test "admin_link_to returns link wrapped in <li> tag" do
-    flunk 'Not done testing yet.'
+    expected_link = content_tag :li, link_to(@sample_link_label, @sample_link_url, :class => 'admin-button')
+    result = admin_link_to(@sample_link_label, @sample_link_url)
+
+    assert_match expected_link, result
   end
 
-  test "admin_panel creates div with class admin-panel" do
-    flunk 'Not done testing yet.'
-  end
+  # TODO: Not sure how to test `content_for` in helper method.
+  # Keep getting `undefined method `append' for nil:NilClass`.
+  # This issue affects all the rest of these tests...
 
-  test "admin_panel contains <ul> with class admin-nav-global" do
-    flunk 'Not done testing yet.'
-  end
+  # test "admin_panel creates div with class admin-panel" do
+  #   content_for :admin_nav_global, "something"
+  #   content_for :admin_nav_local, "something"
+  #   content_for :admin_nav_message, "something"
+  #
+  #   assert_match /class="admin-nav"/, admin_panel
+  # end
 
-  test "admin_panel contains <ul> with class admin-nav-local" do
-    flunk 'Not done testing yet.'
-  end
+  # test "admin_panel contains <ul> with class admin-nav-global" do
+  #   flunk 'Not done testing yet.'
+  # end
 
-  test "admin_panel contains <div> with class admin-nav-message" do
-    flunk 'Not done testing yet.'
-  end
+  # test "admin_panel contains <ul> with class admin-nav-local" do
+  #   flunk 'Not done testing yet.'
+  # end
 
-  test "set_admin_message inserts new message while destroying previous message" do
-    flunk 'Not done testing yet.'
-  end
+  # test "admin_panel contains <div> with class admin-nav-message" do
+  #   flunk 'Not done testing yet.'
+  # end
 
-  test "local_admin_link_to inserts a new li-wrapped link into admin-nav-local" do
-    flunk 'Not done testing yet.'
-  end
+  # test "set_admin_message inserts new message while destroying previous message" do
+  #   flunk 'Not done testing yet.'
+  # end
 
-  test "global_admin_link_to inserts a new li-wrapped link into admin-nav-global" do
-    flunk 'Not done testing yet.'
-  end
+  # test "local_admin_link_to inserts a new li-wrapped link into admin-nav-local" do
+  #   flunk 'Not done testing yet.'
+  # end
+
+  # test "global_admin_link_to inserts a new li-wrapped link into admin-nav-global" do
+  #   flunk 'Not done testing yet.'
+  # end
 
 end
